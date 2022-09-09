7d8368adac65edae36f813fd53cf03079746b1df
MonsefGuerni <monsef.guerni@polymtl.ca>
2022-09-09 11:29:48 -0400

    Moyenne + billets faits

--- exercice.py
+++ exercice.py
@@ -11,24 +11,49 @@ def orthogonal(v1, v2):
 	v1[1] # Pour accéder au Y
 	pass
 
-def average(values):
+def average(values: list) -> list:
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
-	for v in values:
+	somme_totale=0
+	for v in range (len(values)):
+		somme_totale+= values[v]
+		v+=1
 		pass # La variable v contient une valeur de la liste.
 
+	return (somme_totale/v)
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
-	while value != 0:
-		if value >= 20:
+	valeur_restante = value
+	ones = 0
+	twos = 0
+	fives = 0
+	tens = 0
+	twenties = 0
+	while valeur_restante != 0:
+		if valeur_restante >= 20:
+			twenties = value//20
+			valeur_restante = value - twenties*20
+			#print(value, " value")
 			pass
-		elif value >= 10:
+		elif valeur_restante >= 10:
+			tens = valeur_restante //10
+			valeur_restante = valeur_restante - tens*10
 			pass
-		elif value >= 5:
+		elif valeur_restante >= 5:
+			fives = valeur_restante//5
+			valeur_restante = valeur_restante - fives*5
 			pass
-		elif value >= 1:
+		elif valeur_restante >= 2:
+			twos = valeur_restante//2
+
+			valeur_restante = valeur_restante - twos*2
+			print(valeur_restante, " valeur restante 2")
+			pass
+		elif valeur_restante >= 1 :
+			ones = 1
+			valeur_restante=0
 			pass
 
-	return (twenties, tens, fives, twos, ones);
+	return (twenties, " billets de 20$, ", tens, " billets de 10$ ", fives, " billets de 5$ ", twos, " pieces de 2$ ", ones, " piece de 1$ dollar");
 
 def format_base(value, base, digit_letters):
 	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
